172d538cebe3e0036f36f3716579c313e5e85835
zhaqsylyqq <neennn36@gmail.com>
2024-06-24 14:40:11 +0500

    ive created score counter but bestscore isnt saving, also made some corrections into styles

--- src/App.js
+++ src/App.js
@@ -15,17 +15,41 @@ const App = () => {
 		setImage(shuffled);
 	}
 
+	const [score, setScore] = useState(0);
+	const [bestScore, setBestScore] = useState(0);
+	const [lastClicked, setLastClicked] = useState(null);
+
+	const handleImageClick = (index) => {
+		if (lastClicked===index){
+			setScore(0);
+			setBestScore(null);
+		} else{
+			setScore(score + 1);
+			setLastClicked(index);
+
+			if (score + 1 > bestScore) {
+				setBestScore(score + 1);
+			}
+		}
+		shuffleImages();
+	};
+
 	return(
 		<div className="App">
+			<div className='ScoreBoard'>
+				<p> Your score: {score}</p>
+				<p>Your best score: {bestScore}</p>
+			</div>
 			{image.map((img, index) => (
 				<img
 					key={index}
 					src={img}
 					alt={`image${index + 1}`}
-					onClick={shuffleImages}
-					style={{ cursor: 'pointer', margin: '10px' }}
+					onClick={() => handleImageClick(index)}
+					style={{ cursor: 'pointer' }}
 				/>
 			))}
+
 		</div>
 	)
 
